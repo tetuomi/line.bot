6b7d686b282d71cabc803a26e32996bfa5dd87e6
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2019-04-04 15:41:11 +0900

    try

--- app.js
+++ app.js
@@ -8,39 +8,33 @@ const lineConfig = {
 };
 const lineClient = new line.Client(lineConfig);
 
-function firstMessage(input){
-  return {
-    type: "text",
-    text: "小坂菜緒か広瀬すず♡"
-  };
-}
-
 function createReplyMessage(input) {
   // 3. 画像を返す
   const appUrl = process.env.HEROKU_APP_URL;
   const hands = ["小坂菜緒", "広瀬すず"];
-  let text;
+  const message = [];
   if(hands.indexOf(input) == -1){
-    return{
+    message = {
     type: "image",
     previewImageUrl: `${appUrl}/images/akitake.jpg`,
     originalContentUrl: `${appUrl}/images/akitake.jpg`
     };
   }
   else if(hands.indexOf(input) == 0){
-    return {
+    message = {
     type: "image",
     previewImageUrl: `${appUrl}/images/kosakana.jpeg`,
     originalContentUrl: `${appUrl}/images/kosakana.jpeg`
     };
   }
   else if(hands.indexOf(input) == 1){
-    return {
+    message = {
     type: "image",
     previewImageUrl: `${appUrl}/images/hirosuzu.jpg`,
     originalContentUrl: `${appUrl}/images/hirosuzu.jpg`
     };
   }
+  return message;
   // メッセージオブジェクトに関する公式ドキュメント
   // https://developers.line.me/ja/reference/messaging-api/#message-objects
 }
@@ -55,9 +49,6 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
 
   for (const event of req.body.events) {
     if (event.type === "message" && event.message.type === "text") {
-      const message0 = firstMessage(event.message0.text);
-      lineClient.replyMessage(event.replyToken, message0);
-
       const message = createReplyMessage(event.message.text);
       lineClient.replyMessage(event.replyToken, message);//メッセージを届ける
     }
