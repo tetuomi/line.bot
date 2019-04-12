96a12704cd905d7c0b4aec829d1e240f08cde628
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2019-04-12 19:24:56 +0900

    tyr

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
+const path = require("path");
 const express = require("express");
-const line    = require("@line/bot-sdk");
-const pg      = require("pg");
-const config  = require("./config.json");
-
-const pool = new pg.Pool(config.db.postgres);
+const line = require("@line/bot-sdk");
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret:      process.env.LINE_CHANNEL_SECRET_KEY
+  channelSecret: process.env.LINE_CHANNEL_SECRET
 };
-
 const lineClient = new line.Client(lineConfig);
-const server     = express();
+
+function createReplyMessage(input) {
+  // 2. オウム返しする
+  return {
+    type: "text",
+    // `（バッククォート）で囲った中で${変数名}や${式}を書くと結果が展開される
+    // テンプレートリテラル（Template literal）という文法です
+    text: `${input}、と言いましたね？`
+    // 以下と同じです
+    // text: input + '、と言いましたね？'
+  };
+}
+
+const server = express();
+
+server.use("/images", express.static(path.join(__dirname, "images")));
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
+  // LINEのサーバーに200を返す
   res.sendStatus(200);
+
   for (const event of req.body.events) {
-    if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
-      if (event.message.text == "履歴") {
-        pool.connect((err, client, done) => {
-          const query = "SELECT * FROM messages WHERE user_id = '"+event.source.userId+"';";
-          console.log("query: " + query);
-          client.query(query, (err, result) => {
-            console.log(result);
-            done();
-            let messages = [];
-            for (const row of result.rows) {
-              messages.push({type: "text", text: row.message});
-            }
-            lineClient.replyMessage(event.replyToken, messages.slice(-5));
-          });
-        });
-      }
-      else {
-        pool.connect((err, client, done) => {
-          const query = "INSERT INTO messages (user_id, message) VALUES ("
-            +"'"+event.source.userId+"', '"+event.message.text+"');";
-          console.log("query: " + query);
-          client.query(query, (err, result) => {
-            done();
-            if (!err) {
-              lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
-            }
-          });
-        });
-      }
+    if (event.type === "message" && event.message.type === "text") {
+      const message = createReplyMessage(event.message.text);
+      lineClient.replyMessage(event.replyToken, message);//メッセージを届ける
     }
   }
 });
-//22,37,16
 
-server.listen(process.env.PORT || 8000);
+server.listen(process.env.PORT || 8080);
+
+
+/*function createReplyMessage(input) {
+  const messages = [];
+  let text = "";
+ // let order = 0;
+
+  function Message(text){
+    this.type = "text";
+    this.text = text;
+  }
+
+text = `${input}?`;
+messages.push(new Message(text));
+messages.push(new Message("wwwwwwwwww"));
+console.log(messages);
+
+return messages;
+}*/
