ff5c54606891650371cf03c2f48c6f11bf9ef9ad
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2019-04-12 17:01:04 +0900

    test

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-const path = require("path");
 const express = require("express");
-const line = require("@line/bot-sdk");
+const line    = require("@line/bot-sdk");
+const pg      = require("pg");
+const config  = require("./config.json");
+
+const pool = new pg.Pool(config.db.postgres);
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
+  channelSecret:      process.env.LINE_CHANNEL_SECRET_KEY
 };
+
 const lineClient = new line.Client(lineConfig);
-const order = 0;
-function createReplyMessage(input) {
-if(order === 0){
-  if(input == "次へ"){
-    order++;
-    return {
-      type:"text",
-      text:"success"
-    };
-  }
-}
-else if(order === 1){
-  return {
-    type:"text",
-    text: "nice"
-  };
-}
-else {
-  return{
-    type:"text",
-    text:"error"
-  };
-}
-}
-
-const server = express();
-
-server.use("/images", express.static(path.join(__dirname, "images")));
+const server     = express();
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
-  // LINEのサーバーに200を返す
   res.sendStatus(200);
-
   for (const event of req.body.events) {
-    if (event.type === "message" && event.message.type === "text") {
-      const message = createReplyMessage(event.message.text);
-      lineClient.replyMessage(event.replyToken, message);//メッセージを届ける
+    if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
+      if (event.message.text == "履歴") {
+        pool.connect((err, client, done) => {
+          const query = "SELECT * FROM talk WHERE user_id = '"+event.source.userId+"';";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            console.log(result);
+            done();
+            let messages = [];
+            for (const row of result.rows) {
+              messages.push({type: "text", text: row.message});
+            }
+            lineClient.replyMessage(event.replyToken, messages.slice(-5));
+          });
+        });
+      }
+      else {
+        pool.connect((err, client, done) => {
+          const query = "INSERT INTO talk (user_id, message) VALUES ("
+            +"'"+event.source.userId+"', '"+event.message.text+"');";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            done();
+            if (!err) {
+              lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
+            }
+          });
+        });
+      }
     }
   }
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8000);
