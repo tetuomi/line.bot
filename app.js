340dac6f1e1f143aed92c55e43a51c33d6e1abc5
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2019-04-14 11:43:35 +0900

    try

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-const express = require("express");
-const line    = require("@line/bot-sdk");
-const pg      = require("pg");
-const config  = require("./config.json");
+'use strict';
 
-const pool = new pg.Pool(config.db.postgres);
+const express = require('express');
+const line = require('@line/bot-sdk');
+const axios = require('axios');
+const PORT = process.env.PORT || 3000;
 
-const lineConfig = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret:      process.env.LINE_CHANNEL_SECRET
+const config = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
-const lineClient = new line.Client(lineConfig);
-const server     = express();
+const app = express();
 
-server.post("/webhook", line.middleware(lineConfig), (req, res) => {
-  res.sendStatus(200);
-  for (const event of req.body.events) {
-    if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
-      if (event.message.text == "履歴") {
-        pool.connect((err, client, done) => {
-          const query = "SELECT * FROM talk WHERE user_id = '"+event.source.userId+"';";
-          console.log("query: " + query);
-          client.query(query, (err, result) => {
-            console.log(result);
-            done();
-            let messages = [];
-            for (const row of result.rows) {
-              messages.push({type: "text", text: row.message});
-            }
-            lineClient.replyMessage(event.replyToken, messages.slice(-5));
-          });
-        });
-      }
-      else {
-        pool.connect((err, client, done) => {
-          const query = "INSERT INTO talk (user_id, message) VALUES ("
-            +"'"+event.source.userId+"', '"+event.message.text+"');";
-          console.log("query: " + query);
-          client.query(query, (err, result) => {
-            done();
-            if (!err) {
-              lineClient.replyMessage(event.replyToken, {type: "text", text: "記録しました。"});
-            }
-          });
-        });
-      }
-    }
-  }
+app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
 });
-//22,37,16
 
-server.listen(process.env.PORT || 8000);
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  let mes = ''
+  if(event.message.text === '天気教えて！'){
+    mes = 'ちょっとまってね'; //待ってねってメッセージだけ先に処理
+    getNodeVer(event.source.userId); //スクレイピング処理が終わったらプッシュメッセージ
+  }else{
+    mes = event.message.text;
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: mes
+  });
+}
+
+const getNodeVer = async (userId) => {
+    const res = await axios.get('http://weather.livedoor.com/forecast/webservice/json/v1?city=400040');
+    const item = res.data;
+
+    await client.pushMessage(userId, {
+        type: 'text',
+        text: item.description.text,
+    });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
