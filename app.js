1dc9de4c4437bdef82c9f7bd5e6cacf5859c0291
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2019-04-18 19:48:00 +0900

    test

--- app.js
+++ app.js
@@ -22,42 +22,48 @@ function TextMessages(text){
     text: text
   };
 };
-
-if(input == "単語"){
-  messages.push(Textmessages(questions[0]));
-  //numに０を保存
-  pool.connect((err, client, done) => {
-    const query = "INSERT INTO words (user_id, num) VALUES ("
-      +"'"+event.source.userId+"', '"+ 0 +"');";
-    console.log("query: " + query);
-    client.query(query, (err, result) => {
-      done();
-      if (!err) {
-        lineClient.replyMessage(event.replyToken, TextMessages(questions[0]));
+server.post("/webhook", line.middleware(lineConfig), (req, res) => {
+  res.sendStatus(200);
+  for (const event of req.body.events) {
+    if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
+      if(input == "単語"){
+        messages.push(Textmessages(questions[0]));
+        //numに０を保存
+        pool.connect((err, client, done) => {
+          const query = "INSERT INTO words (user_id, num) VALUES ("
+            +"'"+event.source.userId+"', '"+ 0 +"');";
+          console.log("query: " + query);
+          client.query(query, (err, result) => {
+            done();
+            if (!err) {
+              lineClient.replyMessage(event.replyToken, TextMessages(questions[0]));
+            }
+          });
+        });
       }
-    });
-  });
+      else{
+        let x;
+        //numの取り出し
+        pool.connect((err, client, done) => {
+          const query = "SELECT * FROM words WHERE user_id = '"+event.source.userId+"';";
+          client.query(query, (err, result) => {
+            done();
+            messages.push((event.message.text == answers[result.rows.num])? Textmessages("大正解！！") : TextMessages("ぶ～～～"));
+            messages.push(Textmessages(answers[result.rows.num]));
+            messages.push((event.message.text == answers[result.rows.num])? TextMessages(questions[result.rows.num + 1]) : Textmessages(questions[result.rows.num]));
+            lineClient.replyMessage(event.replyToken, messages);
+            x =(event.message.text == answers[result.rows.num])? result.rows.num + 1 : result.rows.num;
+          });
+        });
+        //numの保存
+          pool.connect((err, client, done) => {
+          const query = "INSERT INTO words (user_id, num) VALUES ("
+            +"'"+event.source.userId+"', '"+ x +"');";
+          client.query(query);//大幅に変えた
+        });
 }
-else{
-  let x;
-  //numの取り出し
-  pool.connect((err, client, done) => {
-    const query = "SELECT * FROM words WHERE user_id = '"+event.source.userId+"';";
-    client.query(query, (err, result) => {
-      done();
-      messages.push((event.message.text == answers[result.rows.num])? Textmessages("大正解！！") : TextMessages("ぶ～～～"));
-      messages.push(Textmessages(answers[result.rows.num]));
-      messages.push((event.message.text == answers[result.rows.num])? TextMessages(questions[result.rows.num + 1]) : Textmessages(questions[result.rows.num]));
-      lineClient.replyMessage(event.replyToken, messages);
-      x =(event.message.text == answers[result.rows.num])? result.rows.num + 1 : result.rows.num;
-    });
-  });
-  //numの保存
-    pool.connect((err, client, done) => {
-    const query = "INSERT INTO words (user_id, num) VALUES ("
-      +"'"+event.source.userId+"', '"+ x +"');";
-    client.query(query);//大幅に変えた
-  });
-}
+    }
+  }
+});
 
 server.listen(process.env.PORT || 8000);
