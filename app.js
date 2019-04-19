64ec2ce723df5732cc07ee21d2509f6da8dcb8a1
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2019-04-19 17:37:01 +0900

    ryt

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   for (const event of req.body.events) {
     if (event.source.type == "user" && event.type == "message" && event.message.type == "text") {
       if(event.message.text == "単語"){
-        messages.push(Textmessages(questions[0]));
+        messages.push(TextMessages(questions[0]));
         //numに０を保存
         pool1.connect((err, client, done) => {
           const query = "INSERT INTO words (user_id, num) VALUES ("
@@ -51,10 +51,10 @@ server.post("/webhook", line.middleware(lineConfig), (req, res) => {
           client.query(query, (err, result) => {
             done();
             messages.push((event.message.text == answers[result.rows.num])?
-              Textmessages("大正解！！") : TextMessages("ぶ～～～"));
-            messages.push(Textmessages(answers[result.rows.num]));
+              TextMessages("大正解！！") : TextMessages("ぶ～～～"));
+            messages.push(TextMessages(answers[result.rows.num]));
             messages.push((event.message.text == answers[result.rows.num])? 
-              TextMessages(questions[result.rows.num + 1]) : Textmessages(questions[result.rows.num]));
+              TextMessages(questions[result.rows.num + 1]) : TextMessages(questions[result.rows.num]));
             lineClient.replyMessage(event.replyToken, messages);
             x =(event.message.text == answers[result.rows.num])? result.rows.num + 1 : result.rows.num;
           });
